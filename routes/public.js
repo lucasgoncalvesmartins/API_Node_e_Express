ce017380092b45680596b164e6467cec116f9197
Lucas Martins <lucascamargogoncalvesmartins@gmail.com>
2026-03-16 17:01:46 -0300

    instalando biblioteca de encryptação de senha bcrypt

--- routes/public.js
+++ routes/public.js
@@ -1,13 +1,34 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+
 import { PrismaClient } from '@prisma/client' 
 
 const prisma = new PrismaClient()
 
 const router = express.Router()
 
-router.post('/cadastro', (req, res) => {
+router.post('/cadastro', async (req, res) => {
     const user = req.body
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
+
+    try{
+    await prisma.user.create({
+        data:{
+            email: user.email,
+            name: user.name,
+            password: hashPassword,
+        }
+    })
+
+
     res.status(201).json(user)
+    }
+    catch(e){
+        res.status(500).json({message: "Erro no servidor, tente novamente"})
+    }
+    
 })
 
 
